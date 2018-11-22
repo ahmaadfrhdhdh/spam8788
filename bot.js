45e9e2b25b486f0079d16f1f89fdf4a54320e3f1
ahmaadfrhdhdh <45124321+ahmaadfrhdhdh@users.noreply.github.com>
2018-11-22 14:23:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1say @!LD artist #4417 ";
+var prefix = "1qsay  ";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430";509730091786567680
-var channel = "509357857666236434";509730091786567684
+var server = "509357857666236430";509718539792547841
+var channel = "509357857666236434";509718539792547843
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , creditDream , **')
     },305);
 })
 
